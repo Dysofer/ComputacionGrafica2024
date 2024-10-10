4aa876e5dbc460cf414ea8d640691efdc90f7d28
Dysofer <dylanalexander.solisfernandez.27@gmail.com>
2024-10-10 11:08:35 -0500

    citygame

--- Ciudad con texturas MAGICVOXEL/src/js/game.js
+++ Ciudad con texturas MAGICVOXEL/src/js/game.js
@@ -1,15 +1,38 @@
-function createUI(){
-
+function createUI() {
     var gui = new dat.GUI();
 
     var param = {
-
-        a: "OBJ",
+        a: "Ronin",
         b: "#FF00FF",
         c: 1
     };
 
-    var g = gui.addFolder('Geometry');
-    g.add(param, 'a', ["Mujer", "Hombre", "Luigi", "Mario", "Ronin"]).name("3D Models");
+    var g = gui.addFolder('Geometria');
+       var player = g.add(param, 'a', ["Mujer", "Hombre", "Luigi", "Mario", "Ronin"]).name("Modelos 3D");
+        player.onChange (function(myPlayer){
+            console.log(myPlayer);
+        });
 
+
+    var l = gui.addFolder('Luces');
+    var colorLight = l.addColor(param, 'b').name("Color de Luz");
+    var intensityLight = l.add(param, 'c').min(0).max(1).step(0.1).name("Intensidad");
+
+    colorLight.onChange(function(colorGet) {
+        console.log(colorGet);
+        light.color.setHex(Number(colorGet.toString().replace('#','0x')));
+    });
+
+    intensityLight.onChange(function(intensityGet) {
+        light.intensity = intensityGet;
+    });
+}
+
+function loadObjMtl(){
+
+    // general Path, nameObj, nameMTL
+
+    var generalPath = "./src/models/obj/myPlayer/";
+    var fileObj = "ronin-1.obj";
+    var fileMtl = "ronin-1.mtl";
 }
